ee1cf142a0f17ebb71617a41fe1e336e4f4532c8
mun7na <munna7.pen@gmail.com>
2024-08-01 21:22:57 +0600

    Delete object practice 2.py

--- object practice 2.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name    # This is an instance variable
-        self.age = age
-person1 = Person("Ayan", 25)
-person2 = Person("Bobby", 30)
-print(person1.name)
-print(person2.age)
